d671bdc1fa8b5174a47cc9d179df932236dcbf31
NotAleksei <7Lazev@gmail.com>
2021-08-06 23:11:06 +0300

    delete useless components.

--- src/containers/MainContainer/MainContaner.tsx
+++ src/containers/MainContainer/MainContaner.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import Header from '../Header/Header';
 import './MainContainer.css';
 
 const MainContainer: React.FC = () => {
   return (
     <div className=''>
-
+      <Header/>
     </div>
   );
 }
