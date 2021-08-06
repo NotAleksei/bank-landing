5c306a22590e2b6f04e7eb7b9d73b6b32733e702
NotAleksei <7Lazev@gmail.com>
2021-08-06 15:42:57 +0300

    First commit

---
+++ src/components/Component/Component.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import {IComponent} from './ITicketCard'
+import './TicketCard.css';
+
+const Component: React.FC<IComponent> = (props) => {
+
+
+  return (
+    <div className="">
+    </div>
+  );
+}
+
+export default Component;
---
+++ src/containers/MainContainer/MainContaner.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import './MainContainer.css';
+
+const MainContainer: React.FC = () => {
+  return (
+    <div className=''>
+
+    </div>
+  );
+}
+
+export default MainContainer;
