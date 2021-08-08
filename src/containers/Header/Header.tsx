36de3c67f21cdf1194d7616751ddede9050541e2
NotAleksei <7Lazev@gmail.com>
2021-08-08 13:14:52 +0300

    finish header blockk, add tabs buttons, add login button

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,10 +1,11 @@
 import './Header.css';
 import logo from '../../img/icon/logo.svg'
+import login from '../../img/icon/login.svg'
 import TabsButton from '../../components/TabsButton/TabsButton';
 
 const Header: React.FC = () => {
 
-  const tabsBtnCaptions = ['Услуги', 'Рассчитать кредит', 'Ковертер валют', 'Контакты'];
+  const tabsBtnCaptions = ['Услуги', 'Рассчитать кредит', 'Конвертер валют', 'Контакты'];
 
   const tabsBtn = tabsBtnCaptions.map(caption => <TabsButton caption={ caption }/>)
 
@@ -13,9 +14,13 @@ const Header: React.FC = () => {
       <div className="header__logo">
         <img className="header__logo_img" src={logo} alt=""/>
         <div className="header__logo_label">ЛИГА Банк</div>
-        <div className="header__logo_tab-buttons">
+      </div>
+      <div className="header__tab-buttons">
           {tabsBtn}
-        </div>
+      </div>
+      <div className="header__login">
+        <img className="header__login_img" src={login} alt=""/>
+        <div className="header__login_label">Войти в интернет банк</div>
       </div>
     </div>
   );
